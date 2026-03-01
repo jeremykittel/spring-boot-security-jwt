c0a833d66dadd1eeef4aad1fbe773ad2edc1129a
Jeremy Kittel <jeremy.kittel@gmail.com>
2026-02-28 21:39:59 -0600

    Remove server definitions from OpenApi configuration for simplification

--- src/main/java/com/ninjasmoke/security/config/OpenApiConfig.java
+++ src/main/java/com/ninjasmoke/security/config/OpenApiConfig.java
@@ -8,31 +8,26 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
-    info =
+        info =
         @Info(
-            contact =
+                contact =
                 @Contact(
-                    name = "Ninja",
-                    email = "contact@ninjasmoke.com",
-                    url = "https://ninjasmoke.com/course"),
-            description = "OpenApi documentation for Spring Security",
-            title = "OpenApi specification - Ninja",
-            version = "1.0",
-            license = @License(name = "Licence name", url = "https://some-url.com"),
-            termsOfService = "Terms of service"),
-    servers = {
-      @Server(description = "Local ENV", url = "http://localhost:8080"),
-      @Server(description = "PROD ENV", url = "https://ninjasmoke.com/course")
-    },
-    security = {@SecurityRequirement(name = "bearerAuth")})
+                        name = "Ninja",
+                        email = "contact@ninjasmoke.com",
+                        url = "https://ninjasmoke.com/course"),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification - Ninja",
+                version = "1.0",
+                license = @License(name = "Licence name", url = "https://some-url.com"),
+                termsOfService = "Terms of service"),
+        security = {@SecurityRequirement(name = "bearerAuth")})
 @SecurityScheme(
-    name = "bearerAuth",
-    description = "JWT auth description",
-    scheme = "bearer",
-    type = SecuritySchemeType.HTTP,
-    bearerFormat = "JWT",
-    in = SecuritySchemeIn.HEADER)
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER)
 public class OpenApiConfig {}
