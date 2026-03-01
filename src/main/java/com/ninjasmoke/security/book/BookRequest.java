44369530ab7a215f4456f977a0c6cb94e2fb39b9
Jeremy Kittel <jeremy.kittel@gmail.com>
2026-02-28 21:50:50 -0600

    Update SecurityConfiguration and BookRequest model:
    - Add `NoArgsConstructor` and `AllArgsConstructor` annotations to `BookRequest`.
    - Update security rules to allow GET requests and restrict POST requests to `/api/v1/books/**`.

--- src/main/java/com/ninjasmoke/security/book/BookRequest.java
+++ src/main/java/com/ninjasmoke/security/book/BookRequest.java
@@ -1,12 +1,12 @@
 package com.ninjasmoke.security.book;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookRequest {
 
   private Integer id;
